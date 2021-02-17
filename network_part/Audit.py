6a37e5bca70f2ce4e0efc1dc683c1024b1d6c3d0
OsamaShenoda <osama.samaan.hanna@gmail.com>
2021-02-17 21:35:07 +0300

    update
    update

--- network_part/Audit.py
+++ network_part/Audit.py
@@ -1,12 +1,44 @@
 #!/usr/bin/env python3
 from scapy.all import *
+from datetime import datetime
+import os
+
+
+logFPath = "./logs/packets.log"
 
 nic = "ens33"
 myFilter = 'dst port 5555'
+
+
+import logging
+import time
+from logging.handlers import RotatingFileHandler
+#----------------------------------------------------------------------
+def create_rotating_log(path, log):
+    """
+    Creates a rotating log
+    maxBytes = 880    #5M 5242880
+    backupCount = 100 ## max number of log files to keep before rotate
+    """
+    logger = logging.getLogger("Rotating Log")
+    logger.setLevel(logging.INFO)
+    
+    # add a rotating handler
+    handler = RotatingFileHandler(path, maxBytes=5242880,
+                                  backupCount=100)
+    logger.addHandler(handler)
+    logger.info(log)
+         
+#----------------------------------------------------------------------
+
+    
+   
 def call_back(packet):
     #print(packet.summary())
     #print(type(packet[TCP].payload))
     myPacket = {}
+    logTime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    myPacket["timeStamp"] = logTime
     myPacket["srcIP"] = packet[IP].src
     myPacket["srcPort"] = packet[TCP].sport
     myPacket["dstIP"] = packet[IP].dst
@@ -14,8 +46,10 @@ def call_back(packet):
     myPacket["rawPayload"] = str(packet[TCP].payload)
     if str(type(packet[TCP].payload)) == "<class 'scapy.packet.Raw'>":
         print(myPacket)
-    
-      
-    
+        create_rotating_log(logFPath, myPacket)
+        
 
-sniff(iface=nic, filter= myFilter,prn=call_back, store=0, count=0) 
+ 
+if __name__ == "__main__":
+    sniff(iface = nic, filter = myFilter ,prn = call_back, store = 0, count = 0) 
+    
