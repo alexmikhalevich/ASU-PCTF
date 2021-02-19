58be0c3c897234dd11aee7efe3298b56806b4ddb
OsamaShenoda <osama.samaan.hanna@gmail.com>
2021-02-19 21:42:11 +0300

    Update Availability.py

--- network_part/Availability.py
+++ network_part/Availability.py
@@ -1,7 +1,8 @@
-
+#!/usr/bin/env python3
 import requests
 import json
 import os
+from firewall import *
 
 response = requests.get('http://52.8.181.219/api/game/state').json()
 
@@ -12,4 +13,5 @@ service_states = dynamic[0]["service_states"]["5"]
 for i in range(len(service_states)):
     if (service_states[str(i+1)]["service_state"]) != "up":
         os.system("sudo pkill -f Defense.py")
+        flush()
         print("Defense.py terminated")
